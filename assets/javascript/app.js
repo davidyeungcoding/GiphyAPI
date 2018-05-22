0a58d2d9599370672f47634a7b1d3a95fca71f72
yeungpirate <yeungpirate@gmail.com>
2018-05-22 03:14:06 -0700

    gifs won't change states

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topic = ['Christian Pulisic', 'Zlatan Ibrahimovic', 'Leonel Messi'];
+var topic = ['Christian Pulisic', 'Zlatan Ibrahimovic', 'Mesut Ozil'];
 
 $(document).ready(function() {
     
@@ -14,16 +14,25 @@ $(document).ready(function() {
             console.log(queryURL);
             console.log(response);
 
-            var playerDiv = $('<div class="player">');
-            var rating = response.rated;
-            var ratingHolder = $('<p>').text('Rating: ' + rating);
-            playerDiv.append(ratingHolder);
-            var gifURL = response.url;
-            playerDiv.append(gifURL);
-            buttonBin.prepend(playerDiv);
+            var results = response.data;
+
+            for (i = 0; i < results.length; i++) {
+                var playerDiv = $('<div class="player">');
+                var ratingHolder = $('<p>').text('Rating: ' + results[i].rating);
+                var playerGif = $('<img>');
+                playerGif.attr('src', results[i].images.fixed_height_still.url);
+                playerGif.attr('data-still', results[i].images.fixed_height_still.url);
+                playerGif.attr('data-animate', results[i].images.fixed_height.url);
+                playerGif.attr('data-state', 'still')
+                // playerGif.addClass('test')
+                playerDiv.append(ratingHolder);
+                playerDiv.append(playerGif);
+                $('#gifs').append(playerDiv);
+            };
+
         });
     };
-
+    
     function populateButtons() {
         $('#gifs').empty();
         for (i = 0; i < topic.length; i++) {
@@ -32,15 +41,33 @@ $(document).ready(function() {
             btn.attr('dataPlayer', topic[i]);
             btn.text(topic[i]);
             $('#gifs').append(btn);
+            $('#playerSearch').attr('vlaue', '');
         };
     };
-
+    
     $('#addPlayer').click(function(event) {
         event.preventDefault();
         var player = $('#playerSearch').val().trim();
         topic.push(player);
         populateButtons();
     });
+    
+    $('#gifs').click(function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr('data-animate'));
+            // $(this).attr('src', $(this).results[i].images.fixed_height.url);
+            $(this).attr('data-state', 'animate');
+            console.log('still check');
+        }
+        else if (state == 'animate') {
+            $(this).attr('src', $(this).attr('data-still'));
+            // $(this).attr('src', $(this).results[i].images.fixed_height_still.url);
+            $(this).attr('data-state', 'still');
+            console.log('animate check');
+        }
+        console.log('overall click check');
+    })
 
     $(document).on('click', '.playerBtn', playerGifs);
 
